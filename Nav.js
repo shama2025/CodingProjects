d0b5e0bd206db27d5bdae9d74b749edbd1e19a56
shama2025 <101298098+shama2025@users.noreply.github.com>
2022-04-27 08:46:40 -0400

    Create Nav.js

---
+++ Nav.js
@@ -0,0 +1,23 @@
+//navbar for airbnb app on react
+import React from "react"
+import Women from "./images/image12.png"
+import Star from "./images/Star1.png"
+//__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
+export default function Card(){
+    return(
+        <div>
+<img className = "card--women"src = {Women} alt = "" />
+
+
+<div className = "card--stats">
+<img src= {Star} clasName = "card--star"alt = "" />
+                <span>5.0</span>
+                <span className = "gray">(6) • </span>
+                <span className = "gray">USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className = "bold">From $136</span> / person</p>
+        </div>
+       
+    )
+}
